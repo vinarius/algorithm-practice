eb6b425ecca30f5cf860adce655668f19d24aaca
Mark <vindevccm@gmail.com>
2020-06-23 11:12:33 -0400

    solved circle of numbers

---
+++ javascript/circleOfNumber.js
@@ -0,0 +1,12 @@
+// Consider integer numbers from 0 to n - 1 written down along the circle in such a way that the distance between any two neighboring numbers is equal (note that 0 and n - 1 are neighboring, too).
+
+// Given n and firstNumber, find the number which is written in the radially opposite position to firstNumber.
+
+// Example
+
+// For n = 10 and firstNumber = 2, the output should be
+// circleOfNumbers(n, firstNumber) = 7.
+
+function circleOfNumbers(n, firstNumber) {
+    return firstNumber+(n/2) >= n ? firstNumber - (n/2) : firstNumber + (n/2);
+}
