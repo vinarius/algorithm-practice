d081db488e24a253c565e999bd9d6c668d7ca472
Mark <vindevccm@gmail.com>
2020-10-27 17:44:30 -0400

    solved firstDigit

---
+++ javascript/firstDigit.js
@@ -0,0 +1,17 @@
+// Find the leftmost digit that occurs in a given string.
+
+// Example
+
+//     For inputString = "var_1__Int", the output should be
+//     firstDigit(inputString) = '1';
+//     For inputString = "q2q-q", the output should be
+//     firstDigit(inputString) = '2';
+//     For inputString = "0ss", the output should be
+//     firstDigit(inputString) = '0'.
+
+
+function firstDigit(inputString) {
+    const splitString = inputString.split('');
+    const index = splitString.findIndex(el => el.match(/[0-9]/));
+    return inputString[index];
+}
